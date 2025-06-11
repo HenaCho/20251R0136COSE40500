5fd1befa72e31083e1413e07fddb42c80c67fe4d
Hena <hena0914@korea.ac.kr>
2025-06-11 16:51:14 +0900

    Add config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+﻿<?
+    $host = "localhost";
+    $dbid = "db2021320108";                    // 자신의 DB ID
+    $dbpass = "hena0914@naver.com";                  // 자신의 DB Password
+    $dbname = "db2021320108";                  // 해당 프로젝트에서 사용하는 DB명
+?>
