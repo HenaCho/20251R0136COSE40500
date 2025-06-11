d0ca4315161f90b66af39fa4fd51170f7b2ba3a0
Hena <hena0914@korea.ac.kr>
2025-06-11 16:54:07 +0900

    Add expelled_modify.php

---
+++ expelled_modify.php
@@ -0,0 +1,29 @@
+﻿<?
+include "config.php";    //데이터베이스 연결 설정파일
+include "util.php";      //유틸 함수
+
+$conn = dbconnect($host,$dbid,$dbpass,$dbname);
+mysqli_query($conn, "set autocommit = 0");
+mysqli_query($conn, "set session transaction isolation level serializable");
+mysqli_query($conn, "begin");
+
+$student_id = $_POST['student_id'];
+$name = $_POST['name'];
+$expulsion = $_POST['expulsion'];
+
+$result = mysqli_query($conn, "update expelled_member set name = '$name', expulsion = '$expulsion' where student_id = $student_id");
+
+if(!$result)
+{
+	mysqli_query($conn, "rollback");
+    msg('Query Error : '.mysqli_error($conn));
+}
+else
+{
+	mysqli_query($conn, "commit");
+	mysqli_close($conn);
+    s_msg ('성공적으로 수정 되었습니다');
+    echo "<script>location.replace('expelled_list.php');</script>";
+}
+
+?>
