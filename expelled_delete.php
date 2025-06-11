47c40797ab1c788611e0299b2bbd96bb9de32f07
Hena <hena0914@korea.ac.kr>
2025-06-11 16:53:33 +0900

    Add expelled_delete.php

---
+++ expelled_delete.php
@@ -0,0 +1,27 @@
+﻿<?
+include "config.php";    //데이터베이스 연결 설정파일
+include "util.php";      //유틸 함수
+
+$conn = dbconnect($host,$dbid,$dbpass,$dbname);
+mysqli_query($conn, "set autocommit = 0");
+mysqli_query($conn, "set session transaction isolation level serializable");
+mysqli_query($conn, "begin");
+
+$student_id = $_GET['student_id'];
+
+$ret = mysqli_query($conn, "delete from expelled_member where student_id = $student_id");
+
+if(!$ret)
+{
+	mysqli_query($conn, "rollback");
+	msg('Query Error : '.mysqli_error($conn));
+}
+else
+{
+	mysqli_query($conn, "commit");
+	mysqli_close($conn);
+	s_msg ('성공적으로 삭제 되었습니다');
+	echo "<meta http-equiv='refresh' content='0;url=expelled_list.php'>";
+}
+
+?>
